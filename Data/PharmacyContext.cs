3aaf4dfe8f29cabdfff28a1403a6c1e9c03680b4
andreealarisapatrascu <patrascu_larisa98@yahoo.com>
2021-01-08 23:31:29 +0200

    Add project files.

---
+++ Data/PharmacyContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_PatrascuAndreeaLarisa.Data
+{
+    public class PharmacyContext
+    {
+    }
+}
---
+++ Models/Produs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_PatrascuAndreeaLarisa.Models
+{
+    public class Produs
+    {
+        public int ProdusID { get; set; }
+        public string NumeMedicament { get; set; }
+        public int CategorieID { get; set; }
+        public int FurnizorID { get; set; }
+        public string Doza { get; set; }
+        public decimal Pret { get; set; }
+        public DateTime DataExpirare { get; set; }
+
+        public Categorie Categorie { get; set; }
+        public Furnizor Furnizor { get; set; }
+    }
+}
